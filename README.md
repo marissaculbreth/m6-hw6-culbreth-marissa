7e05fce637b2d69b3fb9a19c8ac28945dfa54b70
Marissa Culbreth <mculbreth1996@ufl.edu>
2019-10-03 15:42:04 -0400

    Add javascript requirements to main.js

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 No outside sources were needed when coding the responsive web page.
 
 # javascript
+https://stackoverflow.com/questions/26069477/adding-items-to-a-list-when-a-button-is-pressed-in-javascript
+This link helped me figure out how to set up the last component of this assignment. My button is not appearing where I want it to however. It appears right next to the element, not beneath it.
+
+Parts 2-4 I was able to figure out myself. A lot of times it helps to write down what you are trying to do before coding it. This helped me better understand what needed to happen once the function ran.
--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 //When a user hovers over any one of the boxes, highlight all of the boxes with a different background color
-
-//this is not working
-var boxItems = document.querySelectorAll('section box-grid h2');
-
-function logThis() {
-  for(i = 0; i < boxItems.length; i++) {
-    boxItems[i].classList.toggle('hovercolor');
-  }
-}
-
+var boxItems = document.querySelectorAll("section#box-grid h2");
 for(i = 0; i < boxItems.length; i++) {
-  boxItems[i].addEventListener('hover', logThis);
-}
+  boxItems[i].addEventListener("mouseenter", function () {
+    for(i = 0; i < boxItems.length; i++) {
+      document.querySelectorAll("section#box-grid h2")[i].style.backgroundColor="#aec6cf";
+    }
+  }, false)
+};
 
 
 
@@ -45,4 +40,26 @@ document.getElementById("box-3").addEventListener("click", function() {
 
 
 //When any one of the three boxes are clicked, have a button that says “Start Over” with an href of “#” appear somewhere on the page by removing a hidden class
-var boxItems = document.querySelectorAll('box-grid h2');
+var boxItems = document.querySelectorAll("section#box-grid h2");
+var newButton = document.createElement("button");
+var a = document.createElement("a");
+a.title = "Start Over";
+a.href = "#";
+var linkText = document.createTextNode('Start Over');
+a.appendChild(linkText);
+
+
+for(i = 0; i < boxItems.length; i++) {
+  boxItems[i].addEventListener("click", function () {
+    for(i = 0; i < boxItems.length; i++) {
+      document.getElementById("sub-text").appendChild(linkText);
+    }
+  }, false)
+};
+// querySelectorAll("section#box-grid h2")[i]
+// for (i = 0; i < boxItems.length; i++) {
+//   boxItems[i].addEventListener('click',
+//     newButton.innerHTML="<a href="#">Start Over</a>";
+//     document.getElementById("box-grid").appendChild(newButton);
+//   );
+// }
